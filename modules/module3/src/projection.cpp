3a1495de1e2bdaaabf84551a694ebee1f18d25e8
hyuny223 <hyuny223@gmail.com>
2022-07-07 12:09:21 +0000

    doProjection() in projection.h

--- modules/module3/src/projection.cpp
+++ modules/module3/src/projection.cpp
@@ -1,38 +1,22 @@
 #include "opencv2/opencv.hpp"
 
 #include "projection.h"
+#include "tracking.h"
 
-void Projection::doProjection(std::shared_ptr<Data::KeyFrame> keyFrame)
+void doProjection(std::shared_ptr<Data::KeyFrame> currKeyFrame, std::vector<cv::Point2d>& newFramePoint2d)
 {
     double k[] = {718.856, 0, 607.1928, 0, 718.856, 185.2157, 0, 0, 1};
     cv::Mat K(3,3,CV_64FC1,k);
 
-    std::shared_ptr<Data::KeyFrame> prevKeyFrame = keyFrame->getPrevKeyFrame();
+    auto point3D = currKeyFrame->getFramePoint3d();
 
+    cv::Mat transformMat = currKeyFrame->getTransformMat();
+    cv::Mat rotationMat, translationMat;
+    Frontend::splitTransformMat(transformMat, rotationMat, translationMat);
 
+    double d[] = {0,0,0,0};
+    cv::Mat distCoeffs(4, 1, CV_64F, d);
 
-    // std::vector<cv::Point3d> vFramePoint3d = keyFrame->getFramePoint3d();
-    // std::vector<cv::KeyPoint> vReprojectPoints = keyFrame->getReprojPoints();
+    cv::projectPoints(point3D, rotationMat, transformMat, K, distCoeffs, newFramePoint2d);
 
-    // std::cout << vFramePoint3d.size() << std::endl;
-
-    // vReprojectPoints.reserve(vFramePoint3d.size());
-
-    // for (int i = 0; i < vFramePoint3d.size(); ++i)
-    // {
-
-    //     double x = K.ptr<double>(0)[0] * (vFramePoint3d[i].x / vFramePoint3d[i].z) + K.ptr<double>(0)[2];
-    //     double y = K.ptr<double>(1)[1] * (vFramePoint3d[i].y / vFramePoint3d[i].z) + K.ptr<double>(1)[2];
-
-    //     vReprojectPoints[i].pt = {x, y};
-
-    //     std::vector<cv::Point2d> goodMatches = keyFrame->getGoodMatches();
-
-    //     for (int i = 0; i < goodMatches.size(); ++i)
-    //     {
-    //         // std::cout << "Origin : ("<< goodMatches[i].x << ", " << goodMatches[i].y << ")\n";
-    //         // std::cout << "Reproj : ("<< vReprojectPoints[i].pt.x << ", " << vReprojectPoints[i].pt.y << ")\n";
-    //         // std::cout << "\n\n";
-    //     }
-    // }
 }
